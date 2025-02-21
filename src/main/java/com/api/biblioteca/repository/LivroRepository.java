247d90596c07feae9383e59cc7569f331cb5de65
jordan <jordandimst@gmail.com>
2025-02-20 22:04:52 -0300

    create Livro repository

---
+++ src/main/java/com/api/biblioteca/repository/LivroRepository.java
@@ -0,0 +1,9 @@
+package com.api.biblioteca.repository;
+
+import com.api.biblioteca.domain.Livro;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LivroRepository extends MongoRepository<Livro, Integer> {
+}
